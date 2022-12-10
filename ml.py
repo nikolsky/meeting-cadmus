331e8256955e274a7165a475c4df6a33e0228783
Stas Prikhodko <stas@poised.com>
2022-12-10 18:54:18 +0300

    gcp stt

--- ml.py
+++ ml.py
@@ -1,6 +1,10 @@
 # Imports the Google Cloud Translation library
 import six
 from google.cloud import translate_v2 as translate
+import io
+
+from google.cloud.speech_v2 import SpeechClient
+from google.cloud.speech_v2.types import cloud_speech
 
 import os
 import openai
@@ -58,6 +62,66 @@ def translate_text(text):
     print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
 
 
+
+
+def transcribe_streaming_v2(project_id, recognizer_name, audio_file):
+    # Instantiates a client
+    client = SpeechClient()
+
+    request = cloud_speech.CreateRecognizerRequest(
+        parent=f"projects/{project_id}/locations/global",
+        recognizer_id="recognizer-4",
+        recognizer=cloud_speech.Recognizer(
+            language_codes=["ru-RU"], model="latest_long"
+        ),
+    )
+
+    # Creates a Recognizer
+    # operation = client.create_recognizer(request=request)
+    # recognizer = operation.result()
+
+    # Reads a file as bytes
+    with io.open(audio_file, "rb") as f:
+        content = f.read()
+
+    # In practice, stream should be a generator yielding chunks of audio data
+    chunk_length = 15360
+    stream = [
+        content[start * chunk_length : (start+1) * chunk_length]
+        for start in range(0, len(content) // chunk_length)
+    ]
+    audio_requests = (
+        cloud_speech.StreamingRecognizeRequest(audio=audio) for audio in stream
+    )
+
+    recognition_config = cloud_speech.RecognitionConfig(auto_decoding_config={})
+    streaming_config = cloud_speech.StreamingRecognitionConfig(
+        config=recognition_config
+    )
+    config_request = cloud_speech.StreamingRecognizeRequest(
+        recognizer=recognizer_name, streaming_config=streaming_config
+    )
+
+    def requests(config, audio):
+        yield config
+        for message in audio:
+            yield message
+
+    # Transcribes the audio into text
+    responses_iterator = client.streaming_recognize(
+        requests=requests(config_request, audio_requests)
+    )
+    responses = []
+    for response in responses_iterator:
+        responses.append(response)
+        for result in response.results:
+            print("Transcript: {}".format(result.alternatives[0].transcript))
+
+    return responses
+
+
+
+
 if __name__ == "__main__":
     # translate_text("я люблю тебя")
 
@@ -65,9 +129,10 @@ if __name__ == "__main__":
     #export GOOGLE_APPLICATION_CREDENTIALS="/Users/stanislavivanov/Downloads/my-project-98575-371210-114ac428d015.json"
 
 
-    prompt = """Write love letter to my real girlfriend named Daria using references to anime Naruto"""
-    resp = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=300, temperature=0.9, top_p=1, frequency_penalty=0.0, presence_penalty=0.6)
-    print(resp)
-    text = resp.choices[0].text
-    print(text)
+    #prompt = """Write love letter to my real girlfriend named Daria using references to anime Naruto"""
+    #resp = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=300, temperature=0.9, top_p=1, frequency_penalty=0.0, presence_penalty=0.6)
+    #print(resp)
+    #text = resp.choices[0].text
+    #print(text)
+    print(transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', "stas.wav"))
     
