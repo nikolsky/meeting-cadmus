f7dc46901b52aae1069408b2220e0ff6e7c442cf
Stas Prikhodko <stas@poised.com>
2022-12-10 22:04:23 +0300

    stt open ai

--- ml.py
+++ ml.py
@@ -2,6 +2,7 @@
 import six
 from google.cloud import translate_v2 as translate
 import io
+from tqdm import tqdm
 
 from google.cloud.speech_v2 import SpeechClient
 from google.cloud.speech_v2.types import cloud_speech
@@ -13,32 +14,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 PROJECT_ID = "my-project-98575-371210"
 
-## Initialize Translation client
-#def translate_text(text="YOUR_TEXT_TO_TRANSLATE", project_id="YOUR_PROJECT_ID"):
-#    """Translating Text."""
-#
-#    client = translate.TranslationServiceClient()
-#
-#    location = "global"
-#
-#    parent = f"projects/{project_id}/locations/{location}"
-#
-#    # Translate text from English to French
-#    # Detail on supported types can be found here:
-#    # https://cloud.google.com/translate/docs/supported-formats
-#    response = client.translate_text(
-#        request={
-#            "parent": parent,
-#            "contents": [text],
-#            "mime_type": "text/plain",  # mime types: text/plain, text/html
-#            "source_language_code": "ru-RU",
-#            "target_language_code": "en-US",
-#        }
-#    )
-#
-#    # Display the translation for each input text provided
-#    for translation in response.translations:
-#        print("Translated text: {}".format(translation.translated_text))
 
 def translate_text(text):
     """Translates text into the target language.
@@ -60,6 +35,7 @@ def translate_text(text):
     print(u"Text: {}".format(result["input"]))
     print(u"Translation: {}".format(result["translatedText"]))
     print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+    return result["translatedText"]
 
 
 
@@ -104,7 +80,7 @@ def transcribe_streaming_v2(project_id, recognizer_name, audio_file):
 
     def requests(config, audio):
         yield config
-        for message in audio:
+        for message in tqdm(audio):
             yield message
 
     # Transcribes the audio into text
@@ -120,6 +96,79 @@ def transcribe_streaming_v2(project_id, recognizer_name, audio_file):
     return responses
 
 
+def save_transcript(trans_resp, path):
+    transcripts = []
+    for resp in trans_resp:
+        for result in resp.results:
+            alternative = result.alternatives[0]
+            transcripts.append(f"{alternative.transcript}/{alternative.words[0].start_offset.seconds}/{alternative.words[0].end_offset.seconds}")
+
+    with open(path, 'w') as f:
+        f.write("\n".join(transcripts))
+
+def parse_transcript():
+    all_trans = []
+    with open("artem_transcript.txt") as f:
+        artem_trans = f.read()
+        artem_trans = artem_trans.split("\n")
+    for item in artem_trans:
+        transcript, start,end = item.split("/")
+        start, end = float(start), float(end)
+        all_trans.append(("Artem", transcript, start,end))
+
+    
+    with open("stas_transcript.txt") as f:
+        stas_trans = f.read()
+        stas_trans = stas_trans.split("\n")
+    for item in stas_trans:
+        transcript, start,end = item.split("/")
+        start, end = float(start), float(end)
+        all_trans.append(("Stas", transcript, start,end))
+    
+    all_trans.sort(key=lambda x: x[2])
+    return all_trans
+
+def merge_transcripts(all_trans):
+    new_transcript = []
+    last_person = None
+    for item in all_trans:
+        person, transcript, start, end = item
+
+        if person == last_person:
+            new_transcript.append(transcript)
+        else:
+            new_transcript.append(f"\n{person}: {transcript}")
+        last_person = person
+    new_transcript = "".join(new_transcript)
+    return new_transcript
+
+def save_translated_txt(translated_txt, path):
+    with open(path, 'w') as f:
+        f.write(translated_txt)
+
+def get_summary(text):
+    if len(text.split()) > 2500:
+        raise Exception("Too long text")
+
+    prompt = f"Here is a transcript of the meeting, make a summary: {text}"
+    resp = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500, temperature=0.9, top_p=1, frequency_penalty=0.0, presence_penalty=0.6)
+    return resp.choices[0].text
+
+def get_arrangements(text):
+    if len(text.split()) > 2500:
+        raise Exception("Too long text")
+
+    prompt = f"Here is a transcript of the meeting, write about setted arrangements: {text}"
+    resp = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500, temperature=0.9, top_p=1, frequency_penalty=0.0, presence_penalty=0.6)
+    return resp.choices[0].text
+
+def get_insights(text):
+    if len(text.split()) > 2500:
+        raise Exception("Too long text")
+
+    prompt = f"Here is a transcript of the meeting, provide meeting insights: {text}"
+    resp = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500, temperature=0.9, top_p=1, frequency_penalty=0.0, presence_penalty=0.6)
+    return resp.choices[0].text
 
 
 if __name__ == "__main__":
@@ -134,5 +183,39 @@ if __name__ == "__main__":
     #print(resp)
     #text = resp.choices[0].text
     #print(text)
-    print(transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', "stas.wav"))
+    # trans_resp = transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', "stas.wav")
+    # save_transcript(trans_resp=trans_resp, path="stas_transcript.txt")
+
+    #trans_resp = transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', "artem.wav")
+    #save_transcript(trans_resp=trans_resp, path="artem_transcript.txt")
+
+    # all_trans = parse_transcript()
+    # new_transcript = merge_transcripts(all_trans)
+    # translated_txt = translate_text("%".join(new_transcript.split("\n")))
+    # save_translated_txt(translated_txt, "translated_transcript.txt")
+
+    with open("translated_transcript.txt") as f:
+        translated_txt = f.read()
     
+    translated_txt = translated_txt.replace("&#39;", "")
+
+    print(f"Transcript have: {len(translated_txt.split())} words")
+
+    translated_txt = "\n".join(translated_txt.split("%"))
+
+    ok_words = translated_txt.split()[-2450:]
+    ok_words_len = sum([len(word) for word in ok_words]) + len(ok_words)
+
+    cutted_translated_txt = translated_txt[:ok_words_len]
+
+
+    
+    # summary = get_summary(cutted_translated_txt)
+    # save_translated_txt(summary, "summary.txt")
+    
+    # arrangement = get_arrangements(cutted_translated_txt)
+    # save_translated_txt(arrangement, "arrangements.txt")
+    
+    insights = get_insights(cutted_translated_txt)
+    save_translated_txt(insights, "insights.txt")
+    1/0
