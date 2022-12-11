2dc4532b02e21a2ad9ed358baefe2a091b3ff514
Artem Nikolsky <nikolsky.artem@gmail.com>
2022-12-11 21:16:37 +0300

    Finished merging ML features

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # meeting-cadmus
 AI-based Google Meets and Zoom meeting notes summarizer 
+
+This is the project for $50K Assembly AI Hackathon
+https://hopin.com/events/assemblyai-ai-hackathon
+
+# Project Story
+##What Inspired Us
+As engineering managers and architects, we found ourselves spending a significant amount of time in meetings - on average, 9 hours per week. This time could have been better spent on more productive tasks, but we also realized that we were missing out on key information and insights from these meetings.
+
+That's what inspired us to create the Cadmus project - an automatic follow-up app for Zoom meetings that provides a concise summary of the meeting, highlighting important agreements, potential risks, and valuable insights.
+
+##What We Learned
+As we started working on the Cadmus project, we quickly realized the challenges involved in extracting relevant information from meetings and summarizing them in a way that is easy to understand.
+
+We also learned about the latest technologies in the field, such as Google Cloud AI and OpenAI, which allowed us to support 190 languages and accurately summarize meetings in a fraction of the time they would normally take.
+
+##How We Built the Project
+Building the Cadmus project was a challenging but rewarding process. We started by researching the latest technologies and algorithms in the field of natural language processing and automatic summarization.
+
+Next, we designed and implemented the core functionality of the app, which involved uploading Zoom meetings, processing the audio and transcript, and generating a summary.
+
+Throughout the development process, we faced several challenges, such as dealing with different accents and dialects, but we were able to overcome these challenges through careful testing and optimization.
+
+##Challenges We Faced
+One of the biggest challenges we faced was accurately summarizing meetings with a high degree of accuracy. This involved dealing with different accents, dialects, and speaking styles, as well as ensuring that the summary captured the key points of the meeting without losing important details.
+
+Another challenge was the cost of processing meetings. We wanted to keep the cost per hour of a meeting as low as possible, which required careful optimization of our algorithms and the use of efficient technologies such as Google Cloud AI and OpenAI.
+
+Overall, the Cadmus project was a challenging but rewarding experience, and we are proud of the results we have achieved so far.
+
+Finally, our MVP can accurately extract from the audio transcript summary, insights, arrangements and even risks.
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
-from ml import transcribe_streaming_v2, save_transcript, parse_transcript
+from ml import transcribe_streaming_v2, save_transcript, parse_transcript, merge_transcripts, translate_text, save_txt, get_summary, get_arrangements, get_insights, get_risks
 import re
 
 PROJECT_ID = "my-project-98575-371210"
@@ -12,6 +12,9 @@ PROJECT_ID = "my-project-98575-371210"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
+
+os.environ['OPENAI_API_KEY'] = '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'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "my-project-98575-371210-114ac428d015.json"
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
@@ -21,31 +24,6 @@ ALLOWED_EXTENSIONS = set(['wav', 'mp4', 'm4a'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# @app.route('/', methods=['GET',"POST"])
-# def home():
-#     form = UploadFileForm()
-
-
-#     if form.validate_on_submit():
-#         uploaded_files = request.files.getlist("file")
-#         print(uploaded_files)
-
-
-#         file = form.file.data # First grab the file
-#         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-#         #return "File has been uploaded."
-#         print("filename", file.filename)
-        
-#         trans_resp = transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', "stas.wav")
-#         save_transcript(trans_resp=trans_resp, path="stas_transcript.txt")
-
-#         trans_resp = transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', "artem.wav")
-#         save_transcript(trans_resp=trans_resp, path="artem_transcript.txt")
-
-#         return redirect(url_for('video', VideoPath=file.filename))
-#     return render_template('index.html', form=form)
 
 @app.route('/', methods=['GET','POST'])
 def upload_file():
@@ -58,15 +36,18 @@ def upload_file():
         files = request.files.getlist('files[]')
 
         video_filename = ""
+        actual_video_name = ""
         speaker_to_transcript_path = {}
         for file in files:
             if file and allowed_file(file.filename):
                 print("add file:", file.filename)
 
                 abs_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+                file.save(abs_path)
 
                 if file.filename.startswith('video'):
                     video_filename = file.filename
+                    actual_video_name, _ = os.path.splitext(abs_path)
                     speaker_name = file.filename.split('video',1)[1]
                     speaker_name = re.search(r"[a-zA-Z]*", speaker_name).group()
 
@@ -77,22 +58,52 @@ def upload_file():
                     
                     print("file", file.filename, speaker_name)
                     #print('ffmpeg -i {} -acodec pcm_s16le -ar 16000 {}.wav'.format(abs_path, actual_filename))
-                    os.system('ffmpeg -i {} -acodec pcm_s16le -ar 16000 {}.wav'.format(abs_path, actual_filename))
+                    os.system('ffmpeg -y -i {} -acodec pcm_s16le -ar 16000 {}.wav'.format(abs_path, actual_filename))
                     print("file", file.filename, speaker_name)
 
-                    trans_resp = transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', actual_filename + '.wav')
-                    save_transcript(trans_resp = trans_resp, path = actual_filename + '.txt')
+                    #trans_resp = transcribe_streaming_v2(PROJECT_ID, 'projects/472813974978/locations/global/recognizers/recognizer-4', actual_filename + '.wav')
+                    #save_transcript(trans_resp = trans_resp, path = actual_filename + '.txt')
+                    
                     speaker_to_transcript_path[speaker_name] = actual_filename + '.txt'
-
-
-                #filename = secure_filename(file.filename)
-                #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
        
         all_trans = parse_transcript(speaker_to_transcript_path)
-        # new_transcript = merge_transcripts(all_trans)
-        # translated_txt = translate_text("%".join(new_transcript.split("\n")))
-        # save_translated_txt(translated_txt, "translated_transcript.txt")
+        #print("All transcript", all_trans)
+        new_transcript = merge_transcripts(all_trans)
+        #print("New transcript", new_transcript)
+        translated_txt = translate_text("%".join(new_transcript.split("\n")))
+        #print("Translated text", translated_txt)
+        translated_text_path = actual_video_name + "_translated_transcript.txt"
+        #save_translated_txt(translated_txt, translated_text_path)
+
+        with open(translated_text_path) as f:
+            translated_txt = f.read()
+    
+        translated_txt = translated_txt.replace("&#39;", "")
+        #translated_txt = translated_txt.replace("%", "\n")
+
+        #print(f"Transcript have: {len(translated_txt.split())} words")
+
+        translated_txt = "\n".join(translated_txt.split("%"))
+        # save_txt(translated_txt, actual_video_name + "_transcript.txt")
+
+        ok_words = translated_txt.split()[-2450:]
+        ok_words_len = sum([len(word) for word in ok_words]) + len(ok_words)
+
+        cutted_translated_txt = translated_txt[:ok_words_len]
+        #print("Cutted translated text", cutted_translated_txt)
+
+
+        summary = get_summary(cutted_translated_txt)
+        save_txt(summary, actual_video_name + "_summary.txt")
+        
+        arrangement = get_arrangements(cutted_translated_txt)
+        save_txt(arrangement, actual_video_name + "_arrangements.txt")
+        
+        insights = get_insights(cutted_translated_txt)
+        save_txt(insights, actual_video_name + "_insights.txt")
+
+        risks = get_risks(cutted_translated_txt)
+        save_txt(risks, actual_video_name + "_risks.txt")
 
         flash('File(s) successfully uploaded')
         return redirect(url_for('video', VideoPath=video_filename))
@@ -101,9 +112,9 @@ def upload_file():
 @app.route('/video/<path:VideoPath>')
 def video(VideoPath):
     print(VideoPath)
-
+    actual_video_name, _ = os.path.splitext(VideoPath)
     insights = summary = arrangements = "In progress..."
-    folder_path = app.config['UPLOAD_FOLDER'] + "/" + VideoPath
+    folder_path = app.config['UPLOAD_FOLDER'] + "/" + actual_video_name
     try:
         with open(folder_path + "_insights.txt", "r") as f:
             insights = f.read()
@@ -121,8 +132,20 @@ def video(VideoPath):
             arrangements = f.read()
     except:
         pass
+
+    try:
+        with open(folder_path + "_risks.txt", "r") as f:
+            risks = f.read()
+    except:
+        pass
+
+    try:
+        with open(folder_path + "_transcript.txt", "r") as f:
+            transcript = f.read()
+    except:
+        pass
     
-    return render_template('videoplayer.html', video_path = VideoPath, insights = insights, summary = summary, arrangements = arrangements)
+    return render_template('videoplayer.html', video_path = VideoPath, insights = insights, summary = summary, arrangements = arrangements, risks=risks, transcript=transcript)
 
 
 if __name__ == '__main__':
